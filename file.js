d56cc790117ba244a19f1047c186e5badc49584a
YuniLestarii <123483879+YuniLestarii@users.noreply.github.com>
2023-09-28 21:17:27 +0700

    Add files via upload

---
+++ file.js
@@ -0,0 +1,11 @@
+let NAMA = "YUNI LESTARI";
+let NIM = "20220040006";
+
+console.log("Nama: " + NAMA);
+console.log("NIM: " + NIM);
+
+let angka1 = 29;
+let angka2 = 6;
+
+let jumlah = angka1 + angka2;
+console.log(" Hasil Penjumlahan: " + jumlah);
